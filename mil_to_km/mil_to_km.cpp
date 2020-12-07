80b4fa6ef90be88867b4ba57eff444fd04f87feb
gakawarstone <pinnekerv@gmail.com>
2020-12-07 20:28:40 +0600

    7 commit

--- mil_to_km/mil_to_km.cpp
+++ mil_to_km/mil_to_km.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
+double get_mile();
+double mile_to_km(double d);
+
+
 int main() {
-  const double km_in_mile = 1.609344;
-  double dist_mile, dist_km;
-  cout << "Distance in mile: ";
-  cin >> dist_mile;
-  dist_km = dist_mile * km_in_mile;
+  double dist_mile = get_mile();
+  double dist_km = mile_to_km(dist_mile);
   cout << "Distance in km: " << dist_km << endl;
   return 0;
+}
+
+
+double get_mile() {
+  double d;
+  cout << "Distance in mile: ";
+  cin >> d;
+  return d;
+}
+
+
+double mile_to_km(double d) {
+  const double km_in_mile = 1.609344;
+  return d * km_in_mile;
 }
