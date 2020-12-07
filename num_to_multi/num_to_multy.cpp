81a1b8d375e877cfd240a35f35e8cacff4598021
gakawarstone <pinnekerv@gmail.com>
2020-12-07 15:31:51 +0600

    6 commit

---
+++ num_to_multi/num_to_multy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int32_t d = 2;
+int32_t n;
+
+int main() {
+  cin >> n;
+
+  while (n != 1){
+    if (n % d == 0){
+      cout << d << " ";
+      n /= d;
+    }
+    else{
+      d++;
+    }
+  }
+
+  return 0;
+}
