798f03eb2c0b461dfb37572b0295c7b7e2b4f8f8
gakawarstone <pinnekerv@gmail.com>
2020-12-07 21:48:06 +0600

    9 commit

--- sum_sqr/sum_sqr.cpp
+++ sum_sqr/sum_sqr.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 using namespace std;
 
+bool check(int32_t n);
+void sum_sqr(int32_t n);
 
 int main() {
-  int32_t n, s, i;
+  int32_t n;
   cout << "Number: ";
   cin >> n;
 
-  for (i = 0; i <= n; ++i) {
-    s += n*n;
+  if (check(n)) {
+    sum_sqr(n);
+  } else {
+    cout << "ERROR number not equal" << endl;
   }
 
-  cout << "Sum of sqrs: " << s << endl;
+  return 0;
+}
+
+
+bool check(int32_t n) {
+  if (n <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+void sum_sqr(int32_t n) {
+    int32_t s = 0;
+
+    for (int32_t i = 1; i <= n; ++i) {
+      s += i*i;
+    }
+
+    cout << "Sum of sqrs: " << s << endl;
+    return;
 }
