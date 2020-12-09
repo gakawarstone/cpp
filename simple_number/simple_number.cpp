eda2f13a8830776396def7334397c5adc1637a85
gakawarstone <pinnekerv@gmail.com>
2020-12-09 23:02:21 +0600

    12 commit

--- simple_number/simple_number.cpp
+++ simple_number/simple_number.cpp
@@ -2,17 +2,53 @@
 #include <math.h>
 using namespace std;
 
-int32_t n;
-bool flag = true;
+void show_mass(int32_t *a);
+bool simple(int32_t n);
+void simple_eragosphen(int32_t n);
+
 
 int main() {
+  int32_t n;
   cin >> n;
+  int32_t *A;
+  cout << simple(n) << endl;
+  simple_eragosphen(n);
+  return 0;
+}
+
+
+void show_mass(int32_t *A, int32_t n) {
+  for (int i = 0; i < n; ++i) {
+    cout << A[i] << ' ';
+  }
+
+  cout << endl;
+}
+
+
+bool simple(int32_t n) { // test number for simple flag method
+  bool flag = true;
   for (int d = 2; d < sqrt(n); ++d) {
     if (n % d == 0) {
       flag = false;
     }
   }
 
-  cout << flag << endl;
-  return 0;
+  return flag;
+}
+
+
+void simple_eragosphen(int32_t n) {
+  int32_t A[n];
+  for (int i = 0; i < n; ++i) {
+    A[i] = i + 1;
+  }
+
+  for (int i = 2; i < sqrt(n); ++i) {
+    for (int k = 2*i - 1; k < n; k += i) {
+      A[k] = 0;
+    }
+  }
+
+  show_mass(A, n);
 }
