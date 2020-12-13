975685ddf57e3bd4f09d18129fd89f5929e85961
gakawarstone <pinnekerv@gmail.com>
2020-12-13 17:14:19 +0600

    24 commit

---
+++ tasks/6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iso646.h>
+
+const double KM_IN_MILE = 1.609344;
+
+
+int main() {
+  int32_t km, mil;
+  std::cout << "Kilometrs: ";
+  std::cin >> km; // read kilometrs
+
+  mil = km * KM_IN_MILE;
+  std::cout << "Miles: " << mil << std::endl; // outp miles
+
+  return 0;
+}
