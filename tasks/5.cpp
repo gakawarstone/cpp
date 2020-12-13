f9d985e35085e4aff7e3de7da2e5c07fbcfdae79
gakawarstone <pinnekerv@gmail.com>
2020-12-13 15:07:05 +0600

    21 commit

---
+++ tasks/5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iso646.h>
+
+
+struct pair_t {
+  int32_t x;
+  int32_t y;
+};
+
+int main() {
+  int32_t n; // init how many numbers to write
+  pair_t bnm_pair; // init biniminal k pair
+  bnm_pair.x = 1; // first biniominal k
+
+  std::cin >> n; // read n
+  std::cout << bnm_pair.x; // outp first number
+
+  for (int i = 0; i < n; ++i) {
+    int32_t tmp;
+    bnm_pair.y = bnm_pair.x * (n - i)/(i + 1);
+    std::cout << " " << bnm_pair.y;
+    tmp = bnm_pair.x; // remember old x
+    bnm_pair.x = bnm_pair.y; // y displace x
+    bnm_pair.y = tmp + bnm_pair.y; // y = sum(x, y)
+  }
+
+  std::cout << std::endl;
+
+  return 0;
+}
