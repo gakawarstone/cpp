d3e7b95313485dae6c0ed7fa5e7e790c51deb698
gakawarstone <pinnekerv@gmail.com>
2020-12-14 13:13:49 +0600

    28 commit

---
+++ game/choose_look.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iso646.h>
+
+int8_t choose(int8_t param);
+
+struct look {
+  int8_t race;
+};
+
+
+int main() {
+  std::cout << ">>> ";
+  std::cout << "Это выбор внешности надеюсь он будет не на ";
+  std::cout << "70000 строчек" << std::endl;
+
+  while (1);
+
+  return 0;
+}
