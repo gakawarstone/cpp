ca75862457c7c1b6c8be31b64ecff492ef30c0bb
gakawarstone <pinnekerv@gmail.com>
2020-12-14 13:54:47 +0600

    29 commit

--- game/choose_look.cpp
+++ game/choose_look.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iso646.h>
+#include <string>
 
 int8_t choose(int8_t param);
 
@@ -8,12 +9,39 @@ struct look {
 };
 
 
+struct choice_of {
+   std::string first;
+   std::string second;
+   std::string third;
+   std::string fourth;
+};
+
 int main() {
   std::cout << ">>> ";
   std::cout << "Это выбор внешности надеюсь он будет не на ";
-  std::cout << "70000 строчек" << std::endl;
+  std::cout << "70000 строчек";
+  getchar(); // wait for ENTER
 
-  while (1);
+  std::cout << ">>> ";
+  std::cout << "Давай перейдем к выбору внешности персонажа";
+  getchar(); // wait for ENTER
 
+  std::cout << ">>> ";
+  std::cout << "Для выбора расы напиши число возле которого ";
+  std::cout << "стоит твой выбор";
+  getchar(); // wait for ENTER
+
+  std::cout << ">>> ";
+  std::cout << "1. Негр" << std::endl;
+  std::cout << ">>> ";
+  std::cout << "2. Сущность в виде гномика" << std::endl;
+  std::cout << ">>> ";
+  std::cout << "3. Негро эльф" << std::endl;
+  std::cout << ">>> ";
+  std::cout << "4. Негрованогеноромаруснил" << std::endl;
+  getchar(); // wait for ENTER
+
+
+  while(1);
   return 0;
 }
