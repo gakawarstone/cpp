da8adb36838f6a89dd6ad713b1ee6f311eed891c
Gakawarstone <pinnekerv@gmail.com>
2020-12-17 15:33:57 +0600

    41 commit

--- hack_pent/hack_pent.cpp
+++ hack_pent/hack_pent.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <iso646.h>
-#include <unistd.h>
+#include <iso646.h> // standart "and or not"
+#include <unistd.h> // import sleep function
 
 
 int main() {
@@ -8,29 +8,40 @@ int main() {
   std::cout << "GKrack ready..." << '\n';
   std::cout << "Please enter number from 1 to 10" << '\n';
   std::cout << ">>> ";
-  getchar();
+  getchar(); // wait for any symbol or ENTER
+
   std::cout << "Hack progress....................10%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................20%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds 
+
   std::cout << "Hack progress....................30%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................40%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................50%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................60%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................70%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................80%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress....................90%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack progress...................100%" << '\n';
-  sleep(5);
+  sleep(5); // wait 5 seconds
+
   std::cout << "Hack: successful" << '\n';
-  sleep(5);
   std::cout << "Password: Hello world!" << '\n';
+
   return 0;
 }
